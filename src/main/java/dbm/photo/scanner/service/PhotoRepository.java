f41387d538e97a84f6118362523cb525ea0bb625
Douglas Murata <dbmurata@gmail.com>
2020-05-14 17:27:54 -0700

    Fix photo read from DB.

--- src/main/java/dbm/photo/scanner/service/PhotoRepository.java
+++ src/main/java/dbm/photo/scanner/service/PhotoRepository.java
@@ -45,5 +45,6 @@ class PhotoRepository { //extends MongoRepository<Photo, String> {
         log.info("Saving {}", ((BasicDBList)(photo.get("files"))).get(0));
         //DBObject p = (DBObject)JSON.parse(photo.toJSON());
         collection.update(new BasicDBObject("_id", photo.get("checksum")), photo, true, false);
+        log.info("Saved");
     }
 }
