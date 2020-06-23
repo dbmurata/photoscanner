ad3fc294de369e238e3f6fc9189caef17fbfea5a
Douglas Murata <dbmurata@gmail.com>
2020-06-22 18:11:37 -0700

    Building API - get media

--- src/main/java/dbm/photo/scanner/Application.java
+++ src/main/java/dbm/photo/scanner/Application.java
@@ -1,11 +1,14 @@
 package dbm.photo.scanner;
 
+import dbm.photo.scanner.service.ScannerAPIController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@ComponentScan(basePackageClasses = ScannerAPIController.class)
 public class Application {
 
     public static void main(String[] args) {
